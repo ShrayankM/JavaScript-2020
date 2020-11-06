c81b92a5efb6d52a485f6565d684acc43aea4127
ShrayankM <shraymist@gmail.com>
2020-11-06 20:17:45 +0530

    updated connect four (all files)

--- 06 - Project#4 (Connect Four)/script.js
+++ 06 - Project#4 (Connect Four)/script.js
@@ -9,6 +9,7 @@ let matrix  = [
 
 // TODO Object Elements
 const firstRow = document.querySelectorAll('.row-0');
+const resetBtn = document.querySelector('.reset');
 // const playerOne = document.querySelector('.player-one');
 // const playerTwo = document.querySelector('.player-two');
 
@@ -17,9 +18,15 @@ const N = firstRow.length;
 const R = 5, C = 6;
 let activePlayer = 'one';
 let flag = true;
+let flagTie = false;
 
 function getActiveColor() {
     return activePlayer === 'one' ? 'rgb(219, 17, 17)' : 'rgb(4, 15, 219)';
+    // return activePlayer === 'one' ? 'red' : 'blue';
+}
+
+function getActivePlayer() {
+    return activePlayer;
 }
 
 function changeActivePlayer() {
@@ -81,6 +88,20 @@ function checkStatus() {
     }
 }
 
+function checkEmpty() {
+    let cnt = 0;
+    for (let i = 0; i < R; i++) {
+        for (let j = 0; j < C; j++) {
+            if (matrix[i][j] == 0) cnt++;
+        }
+    }
+
+    flag = cnt == 0 ? false : true;
+    flagTie = flag == false ? true : false;
+
+    if (cnt == 0) return true;
+}
+
 for (let i = 0; i < N; i++) {
     firstRow[i].addEventListener('click', function() {
 
@@ -93,15 +114,52 @@ for (let i = 0; i < N; i++) {
             if (row >= 0 && col >= 0) {
                 matrix[row][col] = activePlayer === 'one' ? 1 : 2;
                 document.querySelector(`#R${row}C${col}`).style.backgroundColor = getActiveColor();
+                // document.querySelector(`#R${row}C${col}`).classList.add(`${getActiveColor()}-color`);
             }
             
             checkStatus();
+            if (flag && checkEmpty()) {
+                let active = getActivePlayer();
+                active = active == 'one' ? 'two' : 'one';
+
+                document.querySelector(`.player-${active}`).classList.add(`player-active-${active}`);
+            }
 
             if (flag)
                 changeActivePlayer();
         }
 
-        if (!flag)
+        if (!flag && !flagTie)
             console.log("WON Player " + activePlayer);
     });
 }
+
+resetBtn.addEventListener('click', function() {
+    for (let i = 0; i < posFill.length; i++) posFill[i] = 4;
+
+    for (let i = 0; i < R; i++)
+        for (let j = 0; j < C; j++)
+            matrix[i][j] = 0;
+    
+    for (let i = 0; i < R; i++) {
+        for (let j = 0; j < C; j++) {
+            document.querySelector(`#R${i}C${j}`).style.removeProperty('background-color');
+        }
+    }
+
+    if (flagTie) {
+        document.querySelector('.player-two').classList.remove('player-active-two');
+    }
+    else {
+        let active = getActivePlayer();
+        console.log(active);
+        if (active === 'two') {
+            changeActivePlayer();
+        }
+    }
+
+    activePlayer = 'one';
+    
+    flag = true;
+    flagTie = false;
+});
