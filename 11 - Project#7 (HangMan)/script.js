b12703f868f45a332928b2e1f28276f97fc9ac42
ShrayankM <shraymist@gmail.com>
2020-11-11 14:41:44 +0530

    updated project#7 (hangman game)

--- 11 - Project#7 (HangMan)/script.js
+++ 11 - Project#7 (HangMan)/script.js
@@ -4,6 +4,9 @@ const dash = document.querySelector('.dash');
 const mean = document.querySelector('.meaning');
 const guess = document.querySelector('.guess');
 const messageWin = document.querySelector('.messageWin');
+const resetBtn = document.querySelector('.reset');
+const hintBtn = document.querySelector('.hint');
+const hintMessage = document.querySelector('.hint-message');
 
 //TODO ---------------WORDS----------- TODO//
 
@@ -32,18 +35,31 @@ const words = [
 
 //TODO ------------------------------- TODO//
 
-function getRandomWord() {
-    let r = Math.trunc(Math.random() * 3) + 1;
-    return words[r].word;
-}
+// ? ---------- GLOBAL Variables -------- ?//
 
-
-console.log(words);
+let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 const chances = [0, 0, 0, 0, 0, 0];
 let chanceIndex = 0;
 
-let flag = true;
+let flag;
+
+let str = '';
+
+let charPressed = '';
+
+let countStars = 0;
+
+let hintm = '';
+
+//? ------------------------------------- ?//
+
+//* ------------- Functions ------------ *//
+
+function getRandomWord() {
+    let r = Math.trunc(Math.random() * 4);
+    return [words[r].word, words[r].hint]; 
+}
 
 function updateChance() {
     if (chanceIndex > 5) return;
@@ -53,32 +69,31 @@ function updateChance() {
     chanceIndex++;
 }
 
-const str = getRandomWord();
-let countStars = str.length;
-
 function checkStatus() {
-    if (chanceIndex > 5)      messageWin.textContent = "LOST";
+    if (chanceIndex > 5)       messageWin.textContent = "LOST";
     else  if (countStars == 0) messageWin.textContent = "WON";
 }
 
-// const meaning = "Vessel that has the ability to float on water.";
-// mean.textContent += meaning;
+function init() {
 
-for (let i = 0; i < str.length; i++) word.textContent += "*" + " "; 
-word.textContent = word.textContent.trim();
+    str = '';
+    flag = true;
+    chanceIndex = 0;
 
-for (let i = 0; i < str.length; i++) dash.textContent += '_' + " ";
-// word.textContent
-dash.textContent = '_ '.repeat(str.length);
+    for (let i = 0; i < 6; i++) chances[i] = 0;
 
-// console.log(dash.textContent, dash.textContent.length);
+    charPressed = '';
+    [str, hintm] = getRandomWord();
+    countStars = str.length;
 
+    for (let i = 0; i < str.length; i++) word.textContent += "*" + " "; 
+    word.textContent = word.textContent.trim();
 
-let charPressed = '';
+    for (let i = 0; i < str.length; i++) dash.textContent += '_' + " ";
 
+    dash.textContent = '_ '.repeat(str.length);
 
-
-let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+}
 
 function createAlphabets() {
     let alpha = document.querySelector('.alphabets');
@@ -91,13 +106,112 @@ function createAlphabets() {
     }
 }
 
+function clickOrPress(charKey, btnsObj) {
+    if (flag && !charPressed.includes(charKey)) {
+        btnsObj.disabled = true;
+        charPressed += charKey;
+        
+        let wordT = String(word.textContent);
+        if (str.includes(charKey)) {
+            btnsObj.classList.add('right');
+            let temp = '';
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] == charKey) {
+                    countStars--;
+                    temp += charKey;
+                }
+                else 
+                    temp += wordT[i * 2];
+            }
 
+            word.textContent = '';
+            for (let i = 0; i < str.length; i++) {
+                word.textContent += temp[i] + ' ';
+            }
+            word.textContent = word.textContent.trim();
 
+            if (countStars == 0 || chanceIndex > 5) {
+                flag = false;
+            }
+        }
+        else {
+            btnsObj.classList.add('wrong');
+            updateChance();
+            if (chanceIndex > 5) flag = false;
+        }
+    }
+}
+
+function addEvents() {
+    for (let i = 0; i < btnObjs.length; i++) {
+        // * Mouse Over Event
+        btnObjs[i].addEventListener('mouseover', function() {
+            btnObjs[i].classList.add('on-hover');
+        });
+
+        //* Mouse Out Event
+        btnObjs[i].addEventListener('mouseout', function() {
+            btnObjs[i].classList.remove('on-hover');
+        });
+    }
+}
+
+function reset() {
+    str = '';
+    hintm = '';
+    flag = true;
+    chanceIndex = 0;
+
+    for (let i = 0; i < 6; i++) chances[i] = 0;
+
+    charPressed = '';
+    [str, hintm] = getRandomWord();
+    countStars = str.length;
+
+    word.textContent = '';
+    dash.textContent = '';
+
+    for (let i = 0; i < str.length; i++) word.textContent += "*" + " "; 
+    word.textContent = word.textContent.trim();
+
+    for (let i = 0; i < str.length; i++) dash.textContent += '_' + " ";
+
+    dash.textContent = '_ '.repeat(str.length);
+
+    for (let i = 0; i < 6; i++) {
+        const btn = document.querySelector(`#B${i}`);
+        if (btn.classList.contains('red')) {
+            btn.classList.remove('red');
+            btn.classList.add('green');
+        }
+    }
+    
+    for (let i = 0; i < alphabets.length; i++) {
+        const btn = document.querySelector(`#${alphabets[i]}`);
+        if (btn.classList.contains('right')) {
+            btn.classList.remove('right');
+        }
+        else if (btn.classList.contains('wrong')) {
+            btn.classList.remove('wrong');
+        }
+
+        if (btn.classList.contains('on-hover')) { 
+            btn.classList.remove('on-hover');
+            btn.disabled = false;
+        }
+    }
+    messageWin.textContent = '';
+    hintMessage.classList.add('hidden');
+}
+
+//* ----------------------------------- *//
+
+init();
 
 createAlphabets();
 const btnObjs = document.querySelectorAll('.btn-a');
 
-console.log(btnObjs.length);
+addEvents();
 
 function addEvents() {
     for (let i = 0; i < btnObjs.length; i++) {
@@ -116,48 +230,10 @@ function addEvents() {
     for (let i = 0; i < btnObjs.length; i++) {
         btnObjs[i].addEventListener('click', function() {
             let charKey = btnObjs[i].id;
-
-            if (flag && !charPressed.includes(charKey)) {
-                btnObjs[i].disabled = true;
-                charPressed += charKey;
-                // guess.textContent += charKey + ', ';
-                let wordT = String(word.textContent);
-                if (str.includes(charKey)) {
-                    btnObjs[i].classList.add('right');
-                    let temp = '';
-                    for (let i = 0; i < str.length; i++) {
-                        // temp += (str[i] == char.key) ? char.key : wordT[i * 2];
-        
-                        if (str[i] == charKey) {
-                            countStars--;
-                            temp += charKey;
-                        }
-                        else 
-                            temp += wordT[i * 2];
-                    }
-        
-                    word.textContent = '';
-                    for (let i = 0; i < str.length; i++) {
-                        word.textContent += temp[i] + ' ';
-                    }
-                    word.textContent = word.textContent.trim();
-        
-                    if (countStars == 0 || chanceIndex > 5) {
-                        flag = false;
-                    }
-                }
-                else {
-                    btnObjs[i].classList.add('wrong');
-                    updateChance();
-                    if (chanceIndex > 5) flag = false;
-                }
-            }
-        
+            clickOrPress(charKey, btnObjs[i]);
             checkStatus();
-            
         });
     }
-
 }
 
 addEvents();
@@ -165,49 +241,18 @@ addEvents();
 
 //* Keyboard EventListener
 document.addEventListener('keypress', function(char) {
-    // console.log(char.key);
-
     let btnpressed = document.querySelector(`#${char.key}`);
 
-    
-    if (flag && !charPressed.includes(char.key)) {
-
-        btnpressed.classList.add('on-hover');
-        btnpressed.disabled = true;
-        charPressed += char.key;
-        // guess.textContent += char.key + ', ';
-        let wordT = String(word.textContent);
-        if (str.includes(char.key)) {
-
-            btnpressed.classList.add('right');
-            let temp = '';
-            for (let i = 0; i < str.length; i++) {
-                // temp += (str[i] == char.key) ? char.key : wordT[i * 2];
-
-                if (str[i] == char.key) {
-                    countStars--;
-                    temp += char.key;
-                }
-                else 
-                    temp += wordT[i * 2];
-            }
-
-            word.textContent = '';
-            for (let i = 0; i < str.length; i++) {
-                word.textContent += temp[i] + ' ';
-            }
-            word.textContent = word.textContent.trim();
-
-            if (countStars == 0 || chanceIndex > 5) {
-                flag = false;
-            }
-        }
-        else {
-            btnpressed.classList.add('wrong');
-            updateChance();
-            if (chanceIndex > 5) flag = false;
-        }
-    }
-
+    clickOrPress(char.key, btnpressed);
+    btnpressed.classList.add('on-hover');
     checkStatus();
 });
+
+resetBtn.addEventListener('click', function() {
+   reset();
+})
+
+hintBtn.addEventListener('click', function() {
+    hintMessage.textContent += `[${hintm}]`;
+    hintMessage.classList.remove('hidden');
+})
