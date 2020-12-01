b51be23650f42e159cb31022cb4a3d19b11b969c
ShrayankM <shraymist@gmail.com>
2020-12-01 20:24:18 +0530

    added mouseclick funtions in js

--- 08 - Project# (Virtual Keyboard)/script.js
+++ 08 - Project# (Virtual Keyboard)/script.js
@@ -39,11 +39,13 @@ document.addEventListener('keydown', function(e) {
     console.log(e.key);
     let currentChar = e.key;
     if (e.key == "Shift") { 
+        console.log(`CapsLOCK = ${capsFlag} ShiftLock = ${shiftFlag}`);
         setContent();
         shiftBtn.classList.toggle('shift-active'); 
     }
     else if (e.key == "CapsLock") { 
-        console.log('caps-active');
+        console.log(`CapsLOCK = ${capsFlag} ShiftLock = ${shiftFlag}`);
+        // console.log('caps-active');
         setCaps();
     }
     else if (e.key == "Enter") {
@@ -87,8 +89,13 @@ document.addEventListener('keydown', function(e) {
         textCnt = 0;
     }
     else {
-        if (capsBtn.classList.contains('caps-active')) text.textContent += currentChar.toUpperCase();
-        else                                           text.textContent += currentChar;
+        if (capsBtn.classList.contains('caps-active') && shiftBtn.classList.contains('shift-active'))                                 text.textContent += currentChar.toLowerCase();
+        else if (capsBtn.classList.contains('caps-active') || shiftBtn.classList.contains('shift-active')) 
+        text.textContent += currentChar.toUpperCase();
+        else if (!capsBtn.classList.contains('caps-active') && shiftBtn.classList.contains('shift-active'))
+        text.textContent += currentChar.toLowerCase();
+        else if (!capsBtn.classList.contains('caps-active') && !shiftBtn.classList.contains('shift-active'))
+        text.textContent += currentChar;
         let asciiChar = e.key.charCodeAt(0);
         document.querySelector(`._${asciiChar}`).classList.add('num-active');
     }
@@ -96,6 +103,7 @@ document.addEventListener('keydown', function(e) {
 
 document.addEventListener('keyup', function(e) {
     if (e.key == "Shift") { 
+        console.log(`CapsLOCK = ${capsFlag} ShiftLock = ${shiftFlag}`);
         setContent();
         shiftBtn.classList.toggle('shift-active');
     }
@@ -129,7 +137,7 @@ function setEnter() {
 
 function setCaps() {
     capsBtn.classList.toggle('caps-active');
-    switchUpper();
+    // switchUpper();
 }
 
 function switchUpper() {
@@ -152,6 +160,8 @@ function setContent() {
     //* Tab Section
     for (let i = 1; i < tab.length; i++) {
         tab[i].textContent = shiftFlag ? tabUpper[i - 1] : tabLower[i - 1];
+
+        tab[i].textContent = (shiftFlag && capsFlag) ? tabLower[i - 1] : tabUpper[i - 1]; 
         tab[i].classList.add(`_${tabUpper[i - 1].charCodeAt(0)}`);
         tab[i].classList.add(`_${tabLower[i - 1].charCodeAt(0)}`);
     }
@@ -164,6 +174,8 @@ function setContent() {
     //* Caps Section
     for (let i = 1; i < cap.length - 1; i++) {
         cap[i].textContent = shiftFlag ? capsUpper[i - 1] : capsLower[i - 1];
+
+        cap[i].textContent = (shiftFlag && capsFlag) ? capsLower[i - 1] : capsUpper[i - 1]; 
         cap[i].classList.add(`_${capsUpper[i - 1].charCodeAt(0)}`);
         cap[i].classList.add(`_${capsLower[i - 1].charCodeAt(0)}`);
     }
@@ -171,8 +183,89 @@ function setContent() {
     //* shift Section
     for (let i = 1; i < shift.length - 1; i++) {
         shift[i].textContent = shiftFlag ? shiftUpper[i - 1] : shiftLower[i - 1];
+
+        shift[i].textContent = (shiftFlag && capsFlag) ? shiftLower[i - 1] : shiftUpper[i - 1]; 
         shift[i].classList.add(`_${shiftUpper[i - 1].charCodeAt(0)}`);
         shift[i].classList.add(`_${shiftLower[i - 1].charCodeAt(0)}`);
     }
     shiftFlag = shiftFlag ? false : true;
+}
+
+//* ------------------------------------------------------------------------------------------------------- *//
+const numBtns = document.querySelectorAll('.num');
+
+for (let i = 0; i < numBtns.length; i++) {
+    numBtns[i].addEventListener('click', function() {
+        // console.log(numBtns[i].classList);
+
+        // * Not Special Character
+        if (numBtns[i].classList.length > 3) {
+            // let u = numBtns[i].classList[2].slice(1);
+            // let l = numBtns[i].classList[3].slice(1);
+
+            // let lowerStr = String.fromCharCode(l);
+            // let upperStr = String.fromCharCode(u);
+
+            let currentChar = numBtns[i].textContent;
+
+            // console.log(u + " " + l);
+            if (capsBtn.classList.contains('caps-active') && shiftBtn.classList.contains('shift-active'))                                 text.textContent += currentChar.toLowerCase();
+            else if (capsBtn.classList.contains('caps-active') || shiftBtn.classList.contains('shift-active')) 
+            text.textContent += currentChar.toUpperCase();
+            else if (!capsBtn.classList.contains('caps-active') && shiftBtn.classList.contains('shift-active'))
+            text.textContent += currentChar.toLowerCase();
+            else if (!capsBtn.classList.contains('caps-active') && !shiftBtn.classList.contains('shift-active'))
+            text.textContent += currentChar.toLowerCase();
+        }
+        else {
+            let type = numBtns[i].classList[1];
+            console.log(type);
+
+            if (type == 'shift') {
+                setContent();
+                shiftBtn.classList.toggle('shift-active'); 
+            }
+            else if (type == 'caps') {
+                setCaps();
+            }
+            else if (type == 'enter') {
+                // setEnter();
+                let newArea = document.createElement('p');
+                newArea.classList.add(`text-area`);
+                newArea.classList.add(`text-area-${++textCnt}`);
+                area.appendChild(newArea);
+                text = newArea;
+            }
+            else if (type == 'backspace') {
+                console.log(textCnt);
+                // setBackSpace();
+                text.textContent = text.textContent.slice(0, text.textContent.length - 1);
+                if (document.querySelector(`.text-area-${textCnt}`).textContent == "") {
+                    if (textCnt > 0) {
+                        text = document.querySelector(`.text-area-${textCnt - 1}`);
+                        document.querySelector(`.text-area-${textCnt}`).remove();
+                        textCnt--;
+                    }
+                }
+            }
+            else if (type == 'spacebar') {
+                text.innerHTML += '&nbsp';
+                // spaceBtn.classList.add('space-active');
+            }
+            else if (type == 'tab') {
+                text.innerHTML += '&nbsp &nbsp';
+            }
+            else if (type == 'clear') {
+                // setClear();
+                text.textContent = "";
+                for (let i = 1; i <= textCnt; i++) {
+                    // document.querySelector(`.text-area-${i}`).textContent = "";
+                    document.querySelector(`.text-area-${i}`).remove();
+                }
+                document.querySelector('.text-area-0').textContent = "";
+                text = document.querySelector('.text-area-0');
+                textCnt = 0;
+            }
+        }
+    })
 }
