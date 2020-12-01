85bd5ad4ed670d9d5fd04399fd97c33421fc2b96
ShrayankM <shraymist@gmail.com>
2020-12-01 18:55:45 +0530

    added more functionality to keyboard.js

--- 08 - Project# (Virtual Keyboard)/script.js
+++ 08 - Project# (Virtual Keyboard)/script.js
@@ -1,5 +1,7 @@
 //* Global Variables
 let shiftFlag = false;
+let capsFlag = true;
+
 const tabLower = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'];
 const tabUpper = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'];
 
@@ -12,33 +14,138 @@ const shiftUpper = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'];
 const numberLower = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
 const numberUpper = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
 
+let textCnt = 0;
+
 // TODO Objects
-const text = document.querySelector('.text-area');
+let text = document.querySelector('.text-area-0');
 const tab = document.querySelector('.tab-section').querySelectorAll('.num');
 const num = document.querySelector('.numbers-section').querySelectorAll('.num');
 const cap = document.querySelector('.caps-section').querySelectorAll('.num');
 const shift = document.querySelector('.shift-section').querySelectorAll('.num');
+const area = document.querySelector('.area');
+
+// TODO Special Buttons
+const capsBtn = document.querySelector('.caps');
+const entBtn  = document.querySelector('.enter');
+const backBtn = document.querySelector('.backspace');
+const spaceBtn = document.querySelector('.spacebar');
+const tabBtn = document.querySelector('.tab');
+const clearBtn = document.querySelector('.clear');
+const shiftBtn = document.querySelector('.shift');
 
 setContent();
 
 document.addEventListener('keydown', function(e) {
-    if (e.key == "Shift") setContent();
-    if (e.key != "Shift") {
-        text.textContent += e.key;
+    console.log(e.key);
+    let currentChar = e.key;
+    if (e.key == "Shift") { 
+        setContent();
+        shiftBtn.classList.toggle('shift-active'); 
+    }
+    else if (e.key == "CapsLock") { 
+        console.log('caps-active');
+        setCaps();
+    }
+    else if (e.key == "Enter") {
+        setEnter();
+        let newArea = document.createElement('p');
+        newArea.classList.add(`text-area`);
+        newArea.classList.add(`text-area-${++textCnt}`);
+        area.appendChild(newArea);
+        text = newArea;
+    }
+    else if (e.key == "Backspace") {
+        console.log(textCnt);
+        setBackSpace();
+        text.textContent = text.textContent.slice(0, text.textContent.length - 1);
+        if (document.querySelector(`.text-area-${textCnt}`).textContent == "") {
+            if (textCnt > 0) {
+                text = document.querySelector(`.text-area-${textCnt - 1}`);
+                document.querySelector(`.text-area-${textCnt}`).remove();
+                textCnt--;
+            }
+        }
+    }
+    else if (e.key == " ") {
+        text.innerHTML += '&nbsp';
+        spaceBtn.classList.add('space-active');
+    }
+    else if (e.key == "Tab") {
+        setTab();
+        text.innerHTML += '&nbsp &nbsp';
+        setTab();
+    }
+    else if (e.key == "Control") {
+        setClear();
+        text.textContent = "";
+        for (let i = 1; i <= textCnt; i++) {
+            // document.querySelector(`.text-area-${i}`).textContent = "";
+            document.querySelector(`.text-area-${i}`).remove();
+        }
+        document.querySelector('.text-area-0').textContent = "";
+        text = document.querySelector('.text-area-0');
+        textCnt = 0;
+    }
+    else {
+        if (capsBtn.classList.contains('caps-active')) text.textContent += currentChar.toUpperCase();
+        else                                           text.textContent += currentChar;
         let asciiChar = e.key.charCodeAt(0);
-        console.log(`._${asciiChar}`);
         document.querySelector(`._${asciiChar}`).classList.add('num-active');
     }
 })
 
 document.addEventListener('keyup', function(e) {
-    if (e.key == "Shift") setContent();
-    if (e.key != "Shift") {
+    if (e.key == "Shift") { 
+        setContent();
+        shiftBtn.classList.toggle('shift-active');
+    }
+    else if (e.key == "Enter") setEnter();
+    else if (e.key == "Backspace") setBackSpace();
+    else if (e.key == "Control") setClear();
+    else if (e.key == " ") {
+        spaceBtn.classList.remove('space-active');
+    }
+    else {
         let asciiChar = e.key.charCodeAt(0);
-        console.log(asciiChar);
         document.querySelector(`._${asciiChar}`).classList.remove('num-active');
     }
 })
+
+function setClear() {
+    clearBtn.classList.toggle('clear-active');
+}
+
+function setTab() {
+    tabBtn.classList.toggle('tab-active');
+}
+
+function setBackSpace() {
+    backBtn.classList.toggle('back-active');
+}
+
+function setEnter() {
+    entBtn.classList.toggle('enter-active');
+}
+
+function setCaps() {
+    capsBtn.classList.toggle('caps-active');
+    switchUpper();
+}
+
+function switchUpper() {
+    for (let i = 1; i < tab.length - 3; i++) {
+        tab[i].textContent = capsFlag ? tabUpper[i - 1] : tabLower[i - 1];
+    }
+
+    for (let i = 1; i < cap.length - 3; i++) {
+        cap[i].textContent = capsFlag ? capsUpper[i - 1] : capsLower[i - 1];
+    }
+
+    for (let i = 1; i < shift.length - 4; i++) {
+        shift[i].textContent = capsFlag ? shiftUpper[i - 1] : shiftLower[i - 1];
+    }
+    capsFlag = capsFlag ? false : true;
+}
 
 function setContent() {
     // console.log('setting');
